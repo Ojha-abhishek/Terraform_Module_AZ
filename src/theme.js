8a223f9e9fe3c1b884fb5de041982a8c10c857a8
Ojha-abhishek <abhishek.ojha114@gmail.com>
2024-04-13 21:32:41 +0530

    my terraform module

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff8c00', // Change this to your primary color
+    },
+    secondary: {
+      main: '#ff8c00', // Change this to your secondary color
+    },
+  },
+});
+
+export default theme;
